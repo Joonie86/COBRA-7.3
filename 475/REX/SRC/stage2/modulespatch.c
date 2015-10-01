9f1b9dd9c89cba362039a2ca200329b6c78e8d83
Joonie86 <Joonie86@users.noreply.github.com>
2015-09-30 23:23:40 -0700

    Reverted changes

--- 475/REX/SRC/stage2/modulespatch.c
+++ 475/REX/SRC/stage2/modulespatch.c
@@ -124,7 +124,7 @@ SprxPatch dex_vsh_patches[] =
 // NPDRM FSELF fix for REBUG's DEBUG VSH. DEX ONLY to fully support npdrm fself files.
 	{ elf1_func1 + elf1_func1_offset, 0xF821FF81, &condition_true },
 	{ elf1_func1 + elf1_func1_offset + 4, 0x7C0802A6, &condition_true },
-	//{ elf1_func2 + elf1_func2_offset, LI(R11, 0), &condition_true }, Not needed
+	{ elf1_func2 + elf1_func2_offset, LI(R11, 0), &condition_true },
 	{ dex_ps2tonet_patch, ORI(R3, R3, 0x8204), &condition_ps2softemu },
 	{ dex_ps2tonet_size_patch, LI(R5, 0x40), &condition_ps2softemu },
 #elif defined(FIRMWARE_4_30) || defined (FIRMWARE_4_75)
