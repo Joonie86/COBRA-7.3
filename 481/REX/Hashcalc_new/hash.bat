347dfb1859664dc08d35e91d64ea6410c18094bf
Joonie86 <HYU2156926@maricopa.edu>
2017-01-05 22:49:46 -0700

    New Hashcalc for statichash

---
+++ 481/REX/Hashcalc_new/hash.bat
@@ -0,0 +1,19 @@
+hashcalc vsh.self
+hashcalc vsh.self.swp
+hashcalc vsh.self.cexsp
+hashcalc bdp_disccheck_plugin.sprx
+hashcalc basic_plugins.sprx
+hashcalc explore_plugin.sprx
+hashcalc explore_category_game.sprx
+hashcalc ps1_emu.self
+hashcalc ps1_netemu.self
+hashcalc game_ext_plugin.sprx
+hashcalc psp_emulator.self
+hashcalc pemucorelib.sprx
+hashcalc emulator_api.sprx
+hashcalc emulator_drm.sprx
+hashcalc libsysutil_savedata_psp.sprx
+hashcalc libfs.sprx
+
+ echo on
+ pause  press any key to exist
