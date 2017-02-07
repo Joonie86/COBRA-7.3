17fe3760f15492fb68a6342029ea799a69ee8158
Joonie86 <HYU2156926@maricopa.edu>
2017-02-06 23:23:24 -0700

    Syncing

--- 481/REX/SRC/lv2/include/lv2/symbols.h
+++ 481/REX/SRC/lv2/include/lv2/symbols.h
@@ -3574,7 +3574,7 @@
 #define patch_func9_offset 								0x4B8      // Done
 #define mem_base2										0x3D90     // Done
 //#define sc870                                           0x231DCC // DEX 4.81
-
+#define sc864											0x22CC88 // DEX 4.81 , Syscall 864
 /* vars */
 #define thread_info_symbol    							0x3A4A30   // Done
 
