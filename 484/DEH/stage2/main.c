3c73ce150ccb5686266a264fc7dd9965e9740895
J00n <Joonie86@users.noreply.github.com>
2019-03-28 12:41:55 -0700

    Bumping version to 8.1

--- 484/DEH/stage2/main.c
+++ 484/DEH/stage2/main.c
@@ -59,7 +59,7 @@
 #define CB_LOCATION_DEX "/dev_blind/rebug/cobra/stage2.dex"
 
 #define COBRA_VERSION		0x0F
-#define COBRA_VERSION_BCD	0x0801
+#define COBRA_VERSION_BCD	0x0810
 
 #if defined(FIRMWARE_4_84DEH)
 #define FIRMWARE_VERSION	0x0484
